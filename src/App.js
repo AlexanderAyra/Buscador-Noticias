999e1905ddb1231e7137ba7749a8556d01bb5859
Alexander <alexander.ayras@gmail.com>
2020-09-11 19:41:27 -0500

    Consultando la API y leyendo los resultados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario';
 
 const App = () =>  {
 
   const [categoria, setCategoria] = useState('')
+  const [noticias, setNoticias] = useState([])
+
+  useEffect(() => {
+
+    const consultaAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=c71b76fa85a8424eacccb2592cd88bdd`
+
+      const respuesta = await fetch(url)
+      const api = await respuesta.json()
+      setNoticias(api.articles);
+    }
+    consultaAPI()
+
+  }, [categoria])
 
   return (
     <div>
